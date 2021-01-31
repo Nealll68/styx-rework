bcf9cb38a1616533ae22efed4e30d93e68a243a2
Nealll68 <hugoducry@gmail.com>
2021-01-31 11:35:14 +0100

    Edit CORS for socket.io

--- app/Services/Ws.ts
+++ app/Services/Ws.ts
@@ -1,13 +1,19 @@
 import { WsInterface } from '@ioc:Styx/Ws'
 import { Server, Socket } from 'socket.io'
 import AdonisServer from '@ioc:Adonis/Core/Server'
+import corsConfig from 'Config/cors'
 
 class Ws implements WsInterface {
   public isReady = false
   public io: Server
 
   public start (callback: (socket: Socket) => void): void {
-    this.io = new Server(AdonisServer.instance!)
+    this.io = new Server(AdonisServer.instance!, {
+      cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+      }
+    })
 
     this.io.on('connection', callback)
 
