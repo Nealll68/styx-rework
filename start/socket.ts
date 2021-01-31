3deb0277a59940a341b871eccca8ff23bd8b69d4
Nealll68 <hugoducry@gmail.com>
2021-01-31 11:49:04 +0100

    Remove test function

--- start/socket.ts
+++ start/socket.ts
@@ -1,9 +1,3 @@
 import Ws from 'App/Services/Ws'
 
-Ws.start((socket) => {
-  socket.emit('news', { hello: 'world' })
-
-  socket.on('otherevent', (data) => {
-    console.log(data)
-  })
-})
+Ws.start(() => {})
