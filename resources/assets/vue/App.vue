4de7e62ba9f7cf13d25f3319cf7a1694418547f0
Nealll68 <hugoducry@gmail.com>
2021-02-22 10:52:44 +0100

    feat: vue app template

---
+++ resources/assets/vue/App.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+  <router-link to="/">Go to Home</router-link>
+  <router-link to="/about">Go to About</router-link>
+  <el-button type="primary">Primary</el-button>
+  <router-view></router-view>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
