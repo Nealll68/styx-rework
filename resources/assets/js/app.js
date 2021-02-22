8c022c6057eceb32622d1cb0546b8e3da24d8fc6
Nealll68 <hugoducry@gmail.com>
2021-02-22 10:53:00 +0100

    feat: init vue with element-plus and router

---
+++ resources/assets/js/app.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import router from './router.js'
+
+import ElementPlus from 'element-plus'
+import '../scss/element-variables.scss'
+
+import App from '../vue/App.vue'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(ElementPlus)
+
+app.mount('#app')
