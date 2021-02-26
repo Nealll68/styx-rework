63e90c8d4fcc558d67f6996369c03d5682e04c69
Nealll68 <hugoducry@gmail.com>
2021-02-26 10:53:05 +0100

    feat: working on vuetify theme

--- resources/assets/js/plugins/vuetify.js
+++ resources/assets/js/plugins/vuetify.js
@@ -5,5 +5,24 @@ import 'vuetify/dist/vuetify.min.css'
 Vue.use(Vuetify)
 
 export default new Vuetify({
-  theme: { dark: true }
+  theme: { 
+    dark: true, 
+    dark: {
+      primary: '#3f72af',
+      secondary: '#424242',
+      accent: '#82B1FF',
+      error: '#E23E57',
+      info: '#009688',
+      success: '#15A846',
+      warning: '#FC8621',
+    }
+  }
 })
+
+/*
+$--color-primary: #3f72af;
+$--color-success: #15A846;
+$--color-warning: #FC8621;
+$--color-danger: #E23E57;
+$--color-info: #009688;
+*/
