ce7e7189f88c6c35f80c93781d9b98c47af92444
Nealll68 <hugoducry@gmail.com>
2021-03-04 21:40:44 +0100

    feat(auth): add routes for auth

--- start/routes.ts
+++ start/routes.ts
@@ -22,27 +22,35 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Route from '@ioc:Adonis/Core/Route'
 import SteamConsole from 'App/Services/Steam/Console'
 
-Route.get('/steam/cancel', async () => {
-  SteamConsole.cancel()
-})
-
-Route
-  .resource('/profile', 'ProfilesController')
-  .apiOnly()
-
-Route.patch('/parameter/:id', 'ParametersController.update')
-
 Route.group(() => {
-  Route.get('/start', 'ArmaController.start')
-  Route.get('/stop', 'ArmaController.stop')
-  Route.get('/restart', 'ArmaController.restart')
-  Route.patch('/update', 'ArmaController.update')
-}).prefix('/server')
 
-Route.group(() => {
-  Route.get('/:profile/:file', 'FilesController.show')
-  Route.put('/:profile/:file', 'FilesController.update')
-}).prefix('/file')
+  Route.post('register', 'AuthController.register')
+  Route.post('login', 'AuthController.login')
+  
+  Route.get('/steam/cancel', async () => {
+    SteamConsole.cancel()
+  })
+  
+  Route
+    .resource('/profile', 'ProfilesController')
+    .apiOnly()
+  
+  Route.patch('/parameter/:id', 'ParametersController.update')
+  
+  Route.group(() => {
+    Route.get('/start', 'ArmaController.start')
+    Route.get('/stop', 'ArmaController.stop')
+    Route.get('/restart', 'ArmaController.restart')
+    Route.patch('/update', 'ArmaController.update')
+  }).prefix('/server')
+  
+  Route.group(() => {
+    Route.get('/:profile/:file', 'FilesController.show')
+    Route.put('/:profile/:file', 'FilesController.update')
+  }).prefix('/file')
+  
+}).prefix('/api')
+
 
 // SPA
 Route.any('*', async ({ view }: HttpContextContract) => view.render('app'))
