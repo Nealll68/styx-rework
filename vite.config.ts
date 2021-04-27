5d63e9b77e5f05c7c99250ab23d92478cf3c8ed1
Nealll68 <hugoducry@gmail.com>
2021-04-27 11:03:07 +0200

    feat: add "@" aliase

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 //import vue from '@vitejs/plugin-vue'
 const { createVuePlugin } = require('vite-plugin-vue2')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, './resources/js')
+      }
+    ]
+  },
+
   plugins: [createVuePlugin()],
 
   root: './resources',
