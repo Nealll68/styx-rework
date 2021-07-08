686411435a15b103a3af89d1477913ae5e252882
Nealll68 <hugoducry@gmail.com>
2021-07-08 16:58:37 +0200

    refactor: add comment to vite config file

--- config/vite.ts
+++ config/vite.ts
@@ -4,12 +4,46 @@ import Env from '@ioc:Adonis/Core/Env'
 import Application from '@ioc:Adonis/Core/Application'
 
 const ArmaConfig: ViteConfigInterface = {
+
+  /*
+  |--------------------------------------------------------------------------
+  | Dev environnment
+  |--------------------------------------------------------------------------
+  | 
+  | Says to vite if we are in dev environnement or not
+  |
+  */
   dev: Env.get('NODE_ENV') === 'development',
 
+
+  /*
+  |--------------------------------------------------------------------------
+  | manifest path
+  |--------------------------------------------------------------------------
+  | 
+  | Default location of the manifest path
+  |
+  */
   manifestPath: Application.publicPath('assets', 'manifest.json'),  
 
+  /*
+  |--------------------------------------------------------------------------
+  | Dev URL
+  |--------------------------------------------------------------------------
+  | 
+  | URL to use for vite when developping
+  |
+  */
   devUrl: 'http://localhost:3000/assets',
 
+  /*
+  |--------------------------------------------------------------------------
+  | Main URL
+  |--------------------------------------------------------------------------
+  | 
+  | URL to the main js file
+  |
+  */
   mainUrl: 'js/app.js'
 }
 
