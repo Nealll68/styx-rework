03273d5b467ed0bbf7e11299fcbda5e06278c03d
Nealll68 <hugoducry@gmail.com>
2021-08-08 19:27:48 +0200

    feat: share auth object in inertia

--- start/inertia.ts
+++ start/inertia.ts
@@ -11,6 +11,10 @@
 import Inertia from '@ioc:EidelLev/Inertia'
 
 Inertia.share({
+  auth: (ctx) => {
+    return ctx.auth.user
+  },
+
   errors: (ctx) => {
     return ctx.session.flashMessages.get('errors')
   },
