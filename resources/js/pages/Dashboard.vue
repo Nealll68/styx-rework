ffca5c792a114174dffeb9a7af582bf86e28744b
Nealll68 <hugoducry@gmail.com>
2021-09-02 21:40:30 +0200

    clean: remove use confirm test

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -2,7 +2,6 @@
   <section>
     <AppHead title="Dashboard"/>
     <h1>Dashboard</h1>
-    <button @click="confirm">Test confirm</button>
   </section>
 </template>
 
@@ -12,8 +11,6 @@ import AppHead from '@/components/AppHead.vue'
 
 import { UserInterface } from '@/interfaces/user'
 
-import { useTestConfirm } from '@/composables/useTestConfirm'
-
 export default defineComponent({
   components: {
     AppHead
@@ -21,22 +18,6 @@ export default defineComponent({
 
   props: {
     auth: Object as PropType<UserInterface>
-  },
-
-  setup() {
-    // const confirm = useTestConfirm()
-    const confirm = async () => {
-      const confirmed = await useTestConfirm({
-        title: 'test',
-        message: 'blgzlgzk'
-      })
-
-      console.log(confirmed)
-    }
-
-    return {
-      confirm
-    }
   }
 })
 </script>
