0e9a798d218893e5f73721055b94403875af2c31
Nealll <hugoducry@gmail.com>
2022-10-01 01:29:20 +0200

    feat(inertia): add success flash message to shared data

--- start/inertia.ts
+++ start/inertia.ts
@@ -12,5 +12,6 @@ import Inertia from '@ioc:EidelLev/Inertia'
 
 Inertia.share({
   errors: (ctx) => ctx.session.flashMessages.get('errors'),
+  success: (ctx) => ctx.session.flashMessages.get('success'),
   user: (ctx) => ctx.auth.user,
 })
