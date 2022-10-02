4bd05d0b22ce502f8764776ab51dbf36a57b231f
Nealll <hugoducry@gmail.com>
2022-10-02 23:13:05 +0200

    types: add configuration interface

---
+++ resources/js/interfaces/configuration.ts
@@ -0,0 +1,11 @@
+export interface ConfigurationInterface {
+  id: number
+  created_at: string
+  updated_at: string
+  a3server_path: string
+  x64: boolean
+  steamcmd_path: string
+  steam_account: string
+  steam_password: string
+  steam_guard: boolean
+}
